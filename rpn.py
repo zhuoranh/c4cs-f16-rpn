31591e8dc20ec84ee46c8da379b71a53fd1d303f
zhuoranh <zhuoranh@umich.edu>
2016-10-07 14:44:50 -0400

    finish

--- rpn.py
+++ rpn.py
@@ -13,6 +13,16 @@ def calculate(string):
 			arg1 = stack.pop()
 			result = arg1 - arg2
 			stack.append(result)
+		elif token == '*':
+			arg1 = stack.pop()
+			arg2 = stack.pop()
+			result = arg1 * arg2
+			stack.append(result)
+		elif token == '/':
+			arg1 = stack.pop()
+			arg2 = stack.pop()
+			result = arg2 / arg1
+			stack.append(result)
 		else:
 			stack.append(int(token)) 
 		print(stack)
--- test_rpn.py
+++ test_rpn.py
@@ -8,7 +8,13 @@ class TestBasics(unittest.TestCase):
         self.assertEqual(2,result)
     def test_subtract(self):
         result = rpn.calculate("5 3 -")
-        self. assertEqual(2,result)
+        self.assertEqual(2,result)
+    def test_multiply(self):
+        result = rpn.calculate("3 4 *")
+        self.assertEqual(12,result)
+    def test_divide(self):
+        result = rpn.calculate("10 2 /")
+        self.assertEqual(5,result)
     def test_toomanythings(self):
         with self.assertRaises(TypeError):
             rpn. calculate("1 2 3 +")
