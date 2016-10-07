d2b3c8a3948286441f61bd789d4bd1ae322428d3
zhuoranh <zhuoranh@umich.edu>
2016-10-07 14:36:32 -0400

    a lot of stuff

--- rpn.py
+++ rpn.py
@@ -1,7 +1,24 @@
 #!/usr/bin/env python3
 
 def calculate(string):
-    pass
+	stack = list()
+	for token in string.split():
+		if token == '+':
+			arg1 = stack.pop()
+			arg2 = stack.pop()
+			result = arg1 + arg2
+			stack.append(result)
+		elif token == '-':
+			arg2 = stack.pop()
+			arg1 = stack.pop()
+			result = arg1 - arg2
+			stack.append(result)
+		else:
+			stack.append(int(token)) 
+		print(stack)
+	if len(stack) !=1:
+		raise TypeError
+	return stack.pop()
 
 def main():
     while True:
--- test_rpn.py
+++ test_rpn.py
@@ -6,3 +6,9 @@ class TestBasics(unittest.TestCase):
     def test_add(self):
         result = rpn.calculate("1 1 +")
         self.assertEqual(2,result)
+    def test_subtract(self):
+        result = rpn.calculate("5 3 -")
+        self. assertEqual(2,result)
+    def test_toomanythings(self):
+        with self.assertRaises(TypeError):
+            rpn. calculate("1 2 3 +")
